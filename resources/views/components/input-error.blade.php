d831409e3edbe192656249f54280e1119ec12b52
lukecode <lucassocorrosilva@gmail.com>
2023-04-08 19:49:23 -0300

    feat: add novos estilos

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -3,7 +3,7 @@
 @if ($messages)
     <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 space-y-1']) }}>
         @foreach ((array) $messages as $message)
-            <li>{{ $message }}</li>
+            <li class="text-red-600 font-bold bg-red-100 border-l-4 p-3 border-red-600">{{ $message }}</li>
         @endforeach
     </ul>
 @endif
