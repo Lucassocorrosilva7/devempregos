1fb74f12e5c9b506002e8c8329657d85c00345b6
lukecode <lucassocorrosilva@gmail.com>
2023-04-21 19:26:26 -0300

    feat: Criando form para cadastrar vagas

--- resources/views/livewire/create-vacancies.blade.php
+++ resources/views/livewire/create-vacancies.blade.php
@@ -1,3 +1,65 @@
-<div>
-    <h1>livewire</h1>
-</div>
+<form action="" class="md:w-1/2 space-y-5">
+    <div>
+        <x-input-label for="title" :value="__('Título Vaga')" />
+
+        <x-text-input id="email" class="block
+         w-full py-2 pl-4" type="title" name="title" :value="old('title')"
+            required placeholder="Título Vaga" />
+        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    </div>
+
+    <div>
+        <x-input-label for="wage" :value="__('Salário Mensal')" />
+        <select id="wage" name="wage"
+            class="rounded-md shadow-sm  border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200focus: ring-opacity-50 w-full">
+            <option value="">---Selecione----</option>
+            <option value="1">Como candidato</option>
+            <option value="2">Como empresa</option>
+        </select>
+    </div>
+    <div>
+        <x-input-label for="wage" :value="__('Categoria')" />
+        <select id="category" name="category"
+            class="rounded-md shadow-sm  border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200focus: ring-opacity-50 w-full">
+            <option value="">---Selecione----</option>
+            <option value="1">Como candidato</option>
+            <option value="2">Como empresa</option>
+        </select>
+    </div>
+
+    <div>
+        <x-input-label for="company" :value="__('Empresa')" />
+
+        <x-text-input id="company" class="block
+         w-full py-2 pl-4" type="text" name="company"
+            :value="old('company')" required placeholder="Empresa: ex. Netflix, Uber , Google" />
+        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    </div>
+
+    <div>
+        <x-input-label for="last_day" :value="__('Último dia para aplicar')" />
+
+        <x-text-input id="last_day" class="block
+         w-full py-2 pl-4" type="date" name="last_day"
+            :value="old('last_day')" required />
+        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    </div>
+
+    <div>
+        <x-input-label for="last_day" :value="__('Descrição da Vaga')" />
+        <textarea name="description" placeholder="Descrição da Vaga"
+            class="rounded-md shadow-sm  border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full h-72"></textarea>
+    </div>
+
+    <div>
+        <x-input-label for="image" :value="__('Imagem')" />
+
+        <x-text-input id="image" class="block
+         w-full" type="file" name="image" />
+        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    </div>
+
+    <x-primary-button class="w-full justify-center bg-gray-800">
+        {{ __('Criar Vaga') }}
+    </x-primary-button>
+</form>
