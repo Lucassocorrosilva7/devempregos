26fa0a5147ec17b3434c74f7ed9a84dbf4682dec
lukecode <lucassocorrosilva@gmail.com>
2023-04-21 19:39:26 -0300

    feat: Criando colunas para salários

---
+++ database/seeders/WageSeeder.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class WageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('wages')->insert([
+            'wage' => 'R$0 - R$499',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('wages')->insert([
+            'wage' => 'R$500 - R$749',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('wages')->insert([
+            'wage' => 'R$750 - R$999',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('wages')->insert([
+            'wage' => 'R$1000 - R$1499',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('wages')->insert([
+            'wage' => 'R$1500 - R$1999',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('wages')->insert([
+            'wage' => 'R$2000 - R$2499',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('wages')->insert([
+            'wage' => 'R$2500 - R$2999',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('wages')->insert([
+            'wage' => '$R3000 - R$4999',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('wages')->insert([
+            'wage' => '+R$5000',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+    }
+}
