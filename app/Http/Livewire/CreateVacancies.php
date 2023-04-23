4207798197baab6731cb669c0064c907f89d8710
lukecode <lucassocorrosilva@gmail.com>
2023-04-23 15:46:15 -0300

    feat: Conectando form com livewire

--- app/Http/Livewire/CreateVacancies.php
+++ app/Http/Livewire/CreateVacancies.php
@@ -2,12 +2,38 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Wage;
 use Livewire\Component;
+use App\Models\Category;
 
 class CreateVacancies extends Component
 {
+    public $title;
+    public $wage;
+    public $category;
+    public $company;
+    public $last_day;
+    public $description;
+    public $image;
+
+    protected $rules = [
+        'title' => 'required|string',
+        'wage_id' => 'required',
+        'category_id' => 'required',
+        'company' => 'required',
+        'last_day' => 'required',
+        'description' => 'required',
+        'image' => 'required',
+    ];
+
+
     public function render()
+
     {
-        return view('livewire.create-vacancies');
+
+        $wages = Wage::all();
+        $categories = Category::all();
+
+        return view('livewire.create-vacancies', ['wages' => $wages, 'categories' => $categories]);
     }
 }
--- resources/views/livewire/create-vacancies.blade.php
+++ resources/views/livewire/create-vacancies.blade.php
@@ -3,14 +3,14 @@
         <x-input-label for="title" :value="__('Título Vaga')" />
 
         <x-text-input id="email" class="block
-         w-full py-2 pl-4" type="title" name="title" :value="old('title')"
-            required placeholder="Título Vaga" />
+         w-full py-2 pl-4" type="title" wire:model="title"
+            :value="old('title')" placeholder="Título Vaga" />
         <x-input-error :messages="$errors->get('email')" class="mt-2" />
     </div>
 
     <div>
         <x-input-label for="wage" :value="__('Salário Mensal')" />
-        <select id="wage" name="wage"
+        <select id="wage" wire:model="wage"
             class="rounded-md shadow-sm  border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200focus: ring-opacity-50 w-full">
             <option value="">-- Selecione --</option>
             @foreach ($wages as $wage)
@@ -20,7 +20,7 @@
     </div>
     <div>
         <x-input-label for="wage" :value="__('Categoria')" />
-        <select id="category" name="category"
+        <select id="category" wire:model="category"
             class="rounded-md shadow-sm  border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200focus: ring-opacity-50 w-full">
             <option value="">---Selecione----</option>
             @foreach ($categories as $category)
@@ -33,9 +33,9 @@
         <x-input-label for="company" :value="__('Empresa')" />
 
         <x-text-input id="company" class="block
-         w-full py-2 pl-4" type="text" name="company"
-            :value="old('company')" required placeholder="Empresa: ex. Netflix, Uber , Google" />
-        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+         w-full py-2 pl-4" type="text" wire:model="company"
+            :value="old('company')" placeholder="Empresa: ex. Netflix, Uber , Google" />
+        <x-input-error :messages="$errors->get('company')" class="mt-2" />
     </div>
 
     <div>
@@ -43,12 +43,12 @@
 
         <x-text-input id="last_day" class="block
          w-full py-2 pl-4" type="date" name="last_day"
-            :value="old('last_day')" required />
-        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            :value="old('last_day')" />
+        <x-input-error :messages="$errors->get('last_day')" class="mt-2" />
     </div>
 
     <div>
-        <x-input-label for="last_day" :value="__('Descrição da Vaga')" />
+        <x-input-label for="description" :value="__('Descrição da Vaga')" />
         <textarea name="description" placeholder="Descrição da Vaga"
             class="rounded-md shadow-sm  border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full h-72"></textarea>
     </div>
@@ -57,8 +57,8 @@
         <x-input-label for="image" :value="__('Imagem')" />
 
         <x-text-input id="image" class="block
-         w-full" type="file" name="image" />
-        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+         w-full" type="file" :wire:model="image" />
+        <x-input-error :messages="$errors->get('image')" class="mt-2" />
     </div>
 
     <x-primary-button class="w-full justify-center bg-gray-800">
